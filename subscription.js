a8a23643a3eae206b1c36165dcd603905ea248c2
Insu-qg <quent.grange@gmail.com>
2025-06-06 12:12:32 +0200

    green

--- subscription.js
+++ subscription.js
@@ -2,7 +2,8 @@ function canRenewSubscription(subscription, currentDate) {
   return subscription.status === 'active' && 
          new Date(subscription.endDate) <= new Date(currentDate) &&
          !subscription.hasBeenRenewed &&
-         !subscription.unpaidDebt
+         !subscription.unpaidDebt &&
+         !subscription.isTrial;
 }
 
 module.exports = { canRenewSubscription };

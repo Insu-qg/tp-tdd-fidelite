7077368d39bc3feba8a70f9ffe0e81c0d4491a5f
Insu-qg <quent.grange@gmail.com>
2025-06-06 11:45:36 +0200

    refacto avec fonction d’analyse qui retourne un objet avec : { totalPoints, bonusApplied }

--- loyalty.js
+++ loyalty.js
@@ -1,26 +1,28 @@
 function calculateLoyaltyPoints(cart) {
   if (!Array.isArray(cart)) return 0;
+  let { amount, total } = analyzeLoyaltyPoints(cart);
+  return total;
+}
 
+function analyzeLoyaltyPoints(cart) {
   let total = 0;
   let amount = 0;
 
   for (const item of cart) {
-    if (typeof item?.price !== 'number' || item?.price < 0 || item === null) 
+    if (typeof item?.price !== "number" || item?.price < 0 || item === null)
       continue; // Skip invalid price entries
     amount += item.price;
 
-    if (item.type === 'standard') {
+    if (item.type === "standard") {
       total += Math.floor(item.price / 10);
-    } else if (item.type === 'premium') {
-      total += Math.floor(item.price / 10) * 2; 
+    } else if (item.type === "premium") {
+      total += Math.floor(item.price / 10) * 2;
     }
   }
   if (amount > 200) {
     total += 10;
   }
-
-
-  return total;
+  return { amount, total };
 }
 
 module.exports = { calculateLoyaltyPoints };

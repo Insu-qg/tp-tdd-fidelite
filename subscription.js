f815637f3ff3bed13801fb598e3392abf388df91
Insu-qg <quent.grange@gmail.com>
2025-06-06 13:34:13 +0200

    green

--- subscription.js
+++ subscription.js
@@ -1,4 +1,14 @@
 function canRenewSubscription(subscription, currentDate) {
+  // Vérification des champs requis
+  if (!subscription || 
+      !subscription.status ||
+      !subscription.endDate ||
+      typeof subscription.hasBeenRenewed === 'undefined' ||
+      typeof subscription.unpaidDebt === 'undefined' ||
+      typeof subscription.isTrial === 'undefined') {
+    return false;
+  }
+
   return subscription.status === 'active' && 
          new Date(subscription.endDate) <= new Date(currentDate) &&
          !subscription.hasBeenRenewed &&
